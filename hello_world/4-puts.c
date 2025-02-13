d92c5ab6c574e5128e8bbb3c6cc184c46a73474e
lnqbat <b.lonqueu98@gmail.com>
2025-02-13 15:33:40 +0100

    First script c puts hello

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /*
 * Write a C program that prints exactly Programming.
 */
-
 int main(void)
-
-
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }

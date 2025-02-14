f13c2785f09b2251d6fdfc404952b7fb7db4dca7
lnqbat <b.lonqueu98@gmail.com>
2025-02-14 10:20:56 +0100

    My first random number

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* Print random number
+*
+* main - print random positiv or negativ number
+*
+* return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("Positive number: %d\n", n);
+
+	else if (n < 0)
+		printf("Negative number: %\n", n);
+	return (0);
+}

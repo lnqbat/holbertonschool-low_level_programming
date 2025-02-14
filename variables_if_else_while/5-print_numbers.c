e65a23602b0fd094bf3a8f4b7dda60f2dca56612
lnqbat <b.lonqueu98@gmail.com>
2025-02-14 16:04:08 +0100

    digit numbers of base 10 starting from 0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - ....
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}

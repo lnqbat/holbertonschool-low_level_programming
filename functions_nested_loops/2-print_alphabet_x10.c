e940c05d51bfc8548b6d7ff9fab5b1f3f97acf1d
lnqbat <b.lonqueu98@gmail.com>
2025-02-17 10:50:29 +0100

    up and fix bug to alphabet x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,16 @@
 void print_alphabet_x10(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		char letter;
+
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
 		if (i != 9)
 			_putchar('\n');
-
 	}
 }

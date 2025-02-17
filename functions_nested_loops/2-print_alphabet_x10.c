a12ebe25939bc12ea4252a5c1b69493a53501fc0
lnqbat <b.lonqueu98@gmail.com>
2025-02-17 10:39:35 +0100

    fix bug alphabet x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - Write a alphabet
+ * print_alphabet_x10 - Write a alphabet x10
  *
  * Return: always 0
  */
@@ -8,11 +8,13 @@ void print_alphabet_x10(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++){
+	for (i = 0; i < 10; i++)
+	{
 
 		char letter;
 
-		for (letter = 'a'; letter <= 'z'; letter++){
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 
 			_putchar(letter);
 		}

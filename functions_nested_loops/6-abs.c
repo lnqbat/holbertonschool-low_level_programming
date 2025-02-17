29c5d7d1ed8cee8e56be5c5b9d4990bf566b3b6b
lnqbat <b.lonqueu98@gmail.com>
2025-02-17 15:22:36 +0100

    fix the absolute value of an integer.

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - that computes the absolute value of an integer.
+ *
+ *@n: number
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+
+{
+	if (n <= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}

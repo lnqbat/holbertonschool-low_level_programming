4aaa529b8215b0c2cadbc79b7c15eb8b28d9d5a9
lnqbat <b.lonqueu98@gmail.com>
2025-02-17 10:34:09 +0100

    Up my aplhabet x10 /n

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet - Write a alphabet
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++){
+
+		char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++){
+
+			_putchar(letter);
+		}
+	if (i != 9)
+		_putchar('\n');
+
+	}
+}

94c6d83e159493f27f8325b6b4ac7113f44e77af
lnqbat <b.lonqueu98@gmail.com>
2025-02-24 13:09:03 +0100

    Fix my print _

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - print the line _
+ *
+ * @n: _
+ *
+ * Return: Always
+ */
+
+void print_line(int n)
+
+{
+	while (n-- > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
+

38713deab4a3d14c76b7073209b4a851db49f6bb
lnqbat <b.lonqueu98@gmail.com>
2025-02-24 16:08:27 +0100

    Ready up my fizz buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - Fizz-buzz
+ *
+ *
+ * Return: Always 0 .
+ */
+
+int main(void)
+
+{
+	int num;
+
+	for (num = 0; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d", num);
+		}
+	}
+	return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,5 +1,7 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 #ifndef main_h
 #define main_h

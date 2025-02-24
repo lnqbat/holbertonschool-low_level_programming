4e13f3491f05306dd230dbdad8104fe95652a626
lnqbat <b.lonqueu98@gmail.com>
2025-02-24 10:52:30 +0100

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - that prints the numbers, from 0 to 9 .
+ *
+ *
+ */
+
+void print_numbers(void)
+
+{
+	int numbers;
+
+	for (numbers = 48; numbers <= 57; numbers++)
+	{
+		_putchar(numbers);
+	}
+	_putchar('\n');
+}

1dbc8165b4037c14e88d930f16ba6f35ecc442b8
lnqbat <b.lonqueu98@gmail.com>
2025-02-25 17:37:05 +0100

    Print string rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print rev
+ *
+ * @s: string
+ */
+
+void print_rev(char *s)
+
+{
+	int n;
+
+	for (n = 0; s[n] ; n++)
+	;
+	for (; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}

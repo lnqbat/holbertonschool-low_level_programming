b4fc33f5aaae73bcc291525590a9a57f31fd4903
lnqbat <b.lonqueu98@gmail.com>
2025-02-26 18:59:01 +0100

    copies the string pointed to by src

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,15 +2,17 @@
 #include "2-strlen.c"
 
 /**
- * *_strcpy - copies the string pointed to by src.
- *
+ * _strcpy - copies the string pointed to by src.
+ * @dest: value final
+ * @src: string
+ * Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
 
 {
 	int string;
-	
+
 	for (string = 0; string <= _strlen(src); string++)
 	{
 		dest[string] = src[string];

003a2b235695d84b12ba08c5be6b0f0ac43a5acf
lnqbat <b.lonqueu98@gmail.com>
2025-02-26 17:09:41 +0100

    fix my prints half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  puts_half - that prints half of a string
+ *
+ *  @str: is a string
+ */
+
+void puts_half(char *str)
+
+{
+	int string;
+	int n;
+	int mid;
+
+	for (string = 0; str[string]; string++)
+	;
+
+		if (string % 2 == 0)
+
+			mid = string / 2;
+
+		else
+			mid = string + 1 / 2;
+
+		for (n = mid; n < string; n++)
+		{
+			_putchar(str[n]);
+		}
+	_putchar('\n');
+}

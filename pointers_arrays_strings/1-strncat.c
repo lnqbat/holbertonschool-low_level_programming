70f6453fe43b6e811250b595eed777992c20ebf4
lnqbat <b.lonqueu98@gmail.com>
2025-02-27 18:31:21 +0100

    the second task two strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ *
+ * @dest: string
+ * @src: string 2
+ * @n: n bytes from src
+ *
+ *  Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int len, count;
+
+	for (len = 0; dest[len] != '\0'; len++)
+	;
+	for (count = 0; src[count] != '\0'; count++)
+	{
+		dest[len] = src[count];
+		len++;
+	}
+	if (count < n)
+	{
+		dest[len] = '\0';
+	}
+	return (dest);
+}

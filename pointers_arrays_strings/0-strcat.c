fb0a39a1da0a064af66e41da26a5d249b5c46028
lnqbat <b.lonqueu98@gmail.com>
2025-02-27 18:13:00 +0100

    update my main and first task

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+	int len, count;
+
+	for (len = 0; dest[len] != '\0'; len++)
+	;
+	for (count = 0; src[count] != '\0'; count++)
+	{
+		dest[len] = src[count];
+		len++;
+	}
+	return (dest);
+}

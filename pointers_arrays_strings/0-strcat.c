3215f7eff2e3e4c3784a5d3ff9b9fa7f0a17310f
lnqbat <b.lonqueu98@gmail.com>
2025-02-27 18:15:40 +0100

    The first task to concatenates two strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
+ * _strcat - concatenates two strings.
  *
+ * @dest: first string
+ * @src: second string
  *
- *
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)

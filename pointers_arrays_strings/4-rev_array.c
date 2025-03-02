7794beec1cf5ce8691b8e176a3503e465982a555
lnqbat <b.lonqueu98@gmail.com>
2025-03-02 15:37:17 +0100

    my reverse array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of integrers.
+ *
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ */
+
+void reverse_array(int *a, int n)
+
+{
+
+	int i, temp;
+
+	while (i = 0, i < n)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+		i++;
+		n--;
+	}
+}

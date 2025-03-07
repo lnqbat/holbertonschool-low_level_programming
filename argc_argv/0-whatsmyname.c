f4b5218422429052ccf11e8172b18dda17613cf7
lnqbat <b.lonqueu98@gmail.com>
2025-03-07 14:28:59 +0100

    my new project on c argc,argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ *
+ * @argc: no used
+ * @argv: string contain
+ *
+ * Return: always 0.
+ */
+
+int main(__attribute__((unused))int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+

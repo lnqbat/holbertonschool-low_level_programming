5e6071bb6b079d6ae18051021656bd5c6a2e9ded
lnqbat <b.lonqueu98@gmail.com>
2025-03-12 09:53:25 +0100

    update header dog

---
+++ structures_typedef/main.h
@@ -0,0 +1,22 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - Struct to referenc dog
+ * @name: name dog
+ * @age: Age dog
+ * @owner: owner a dog
+ *
+ * Description: to referenc a dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
+

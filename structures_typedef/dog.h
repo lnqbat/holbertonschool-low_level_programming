ed4942bb4b20e9db82f9df731734aaa26baa2b51
lnqbat <b.lonqueu98@gmail.com>
2025-03-12 09:41:02 +0100

    my first structures

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef dog_h
+#define dog_h
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
+#endif
+

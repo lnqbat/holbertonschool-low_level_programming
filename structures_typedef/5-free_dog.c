c2395c6804d4c8ef9c37e615bd91d60c6b01f44d
lnqbat <b.lonqueu98@gmail.com>
2025-03-14 12:11:37 +0100

    free my new dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+ #include "dog.h"
+
+/**
+ * free_dog - that frees dogs.
+ *
+ * @d: pointer to a dog.
+ */
+
+void free_dog(dog_t *d)
+
+{
+	if ( d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}

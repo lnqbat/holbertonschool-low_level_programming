10f1c6df569b2b773f8eec6777126f569c321744
lnqbat <b.lonqueu98@gmail.com>
2025-03-17 09:56:28 +0100

    my first task print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ *
+ * @name: pointer char
+ * @f: pointer func
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+
+{
+	if (name == NULL)
+		exit(-1);
+	f(name);
+}

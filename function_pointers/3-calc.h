0b36726c4065375b079eb6808a6d6b73ba903808
lnqbat <b.lonqueu98@gmail.com>
2025-03-17 16:13:30 +0100

    update betty code

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,16 +1,16 @@
 #ifndef CALC_H
 #define CALC_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 /**
  * struct op - Struct op
  *
  * @op: The operator
  * @f: The function associated
  */
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
 typedef struct op
 {
 	char *op;

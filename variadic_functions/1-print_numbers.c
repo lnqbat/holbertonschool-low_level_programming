424c8be2fe8ab02e218f0977c1852104dfd4309d
lnqbat <b.lonqueu98@gmail.com>
2025-03-18 11:26:50 +0100

    my update print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,16 +13,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list num;
 
-	va_start(num, n);
 
 	if (separator == NULL)
-		separator = "";
+		separator = " ";
+
+	va_start(num, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(num, int));
 
-		if (n != n - 1)
+		if (i != n - 1)
 		{
 			printf("%s", separator);
 		}

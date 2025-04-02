e50dad828d80b63df6773169ca70d5892b72c1aa
lnqbat <b.lonqueu98@gmail.com>
2025-04-02 17:22:03 +0200

    update that code function

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,4 @@
  #include "lists.h"
-
 /**
  * insert_dnodeint_at_index - Inserts a node at a given index.
  *
@@ -9,45 +8,45 @@
  *
  * Return: Address of the new node, or NULL if it fails
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *temp = *h;
+	dlistint_t *new_node, *temp = *h;
 	unsigned int i = 0;
 
-	if (!h)
-	{
+	if (h == NULL)
 		return (NULL);
-	}
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
-	{
 		return (NULL);
-	}
-	new_node->n = n;
 
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 	if (idx == 0)
 	{
-		return (add_dnodeint(h, n));
+		if (*h != NULL)
+		{
+			new_node->next = *h;
+			(*h)->prev = new_node;
+		}
+		*h = new_node;
+		return (new_node);
 	}
 	while (temp && i < idx - 1)
 	{
 		temp = temp->next;
 		i++;
 	}
-	if (!temp)
+	if (temp == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	if (!temp->next)
-	{
-		return (add_dnodeint_end(h, n));
-	}
 	new_node->next = temp->next;
 	new_node->prev = temp;
-	temp->next->prev = new_node;
+	if (temp->next)
+		temp->next->prev = new_node;
 	temp->next = new_node;
 
 	return (new_node);
